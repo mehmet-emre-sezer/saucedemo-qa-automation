0214f2308d8bc9ba2cf55afd3a908961d0d08523
mehmet-emre-sezer <sezermehmetemre@gmail.com>
2026-09-16 09:56:06 +0300

    test: add BaseTest with browser setup/teardown
    - @BeforeEach launches Chrome, maximizes, sets implicit wait, opens AUT
    - @AfterEach quits the driver with a null guard

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,29 @@
+package base;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.saucedemo.com");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
+
+}
