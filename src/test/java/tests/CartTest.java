66fe45cebf4987e15ecc72bd38c52a99acbd7cf2
mehmet-emre-sezer <sezermehmetemre@gmail.com>
2026-09-16 19:29:54 +0300

    test: automate TC-CART-003/004 continue shopping and checkout
    - Continue Shopping returns to inventory
    - Checkout (with item in cart) proceeds to checkout step one
    - Completes Cart module automation (4/4 test cases)

--- src/test/java/tests/CartTest.java
+++ src/test/java/tests/CartTest.java
@@ -12,7 +12,7 @@ import pages.ProductsPage;
 public class CartTest extends BaseTest {
 
     @BeforeEach
-    public void loginFirst(){
+    public void loginFirst() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("standard_user", "secret_sauce");
     }
@@ -41,5 +41,30 @@ public class CartTest extends BaseTest {
         CartPage cartPage = new CartPage(driver);
         cartPage.removeItem();
         Assertions.assertEquals(0, cartPage.getCartItemCount());
+    }
+
+    @Test
+    @DisplayName("TC-CART-003 - Continue Shopping returns to Product")
+    void continueShoppingReturnsToProducts() {
+        ProductsPage productsPage = new ProductsPage(driver);
+        productsPage.openCart();
+
+        CartPage cartPage = new CartPage(driver);
+        cartPage.clickContinueShopping();
+        String currentUrl = driver.getCurrentUrl();
+        Assertions.assertEquals("https://www.saucedemo.com/inventory.html", currentUrl);
+    }
+
+    @Test
+    @DisplayName("TC-CART-004 - Checkout proceeds to step 1")
+    void checkoutProceedsToStepOne() {
+        ProductsPage productsPage = new ProductsPage(driver);
+        productsPage.addBackpackToCart();
+        productsPage.openCart();
+
+        CartPage cartPage = new CartPage(driver);
+        cartPage.clickCheckout();
+        String currentUrl = driver.getCurrentUrl();
+        Assertions.assertEquals("https://www.saucedemo.com/checkout-step-one.html", currentUrl);
     }
 }
